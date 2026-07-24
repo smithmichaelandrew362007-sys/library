52dfe603c7326b70a009024e9713de396fea56d6
Smith <smithmichaelandrew362007@gmail.com>
2026-07-24 21:20:05 +0530

    fix(android): Migrate to Kotlin DSL build files and upgrade Gradle to 8.10.2 for Flutter stable compatibility

---
+++ mobile/android/app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.libravault.mobile"
+    compileSdk = flutter.compileSdkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.libravault.mobile"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ mobile/android/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    val flutterSdkPath: String = run {
+        val properties = java.util.Properties()
+        file("local.properties").takeIf { it.exists() }?.inputStream()?.use { properties.load(it) }
+        val sdkPath = properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
+        checkNotNull(sdkPath) { "flutter.sdk not set in local.properties and FLUTTER_ROOT not set" }
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+}
+
+include(":app")
