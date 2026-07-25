af5f116bdcbb1ef92db39c1d1410d62b424da8b9
Smith <smithmichaelandrew362007@gmail.com>
2026-07-25 14:47:54 +0530

    fix(android): Remove custom buildDir redirect - causes Flutter to lose APK output path

--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -5,16 +5,6 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
